c2762131903904b8c6efdd047bfaaeeab2ace947
Thuy Pham <tpham045@hotmail.com>
2016-11-12 21:59:13 +0000

    add collection state and redirect mormalize file

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,29 +1,29 @@
  (function() {
-   function config($stateProvider, $locationProvider) {
-     $locationProvider
-     //disable to hashbang mode like localhost:3000/#!/album
-       .html5Mode({
-       enabled: true,
-       requireBase: false
-     });
-     //add state to direct to correct template
-     $stateProvider
-       .state('landing', {
-         url: '/',
-         templateUrl: '/templates/landing.html'
-       })
-       .state('album', {
-         url: '/album',
-         templateUrl: '/templates/album.html'
-       })
-       .state('collection', {
-         url: '/collection',
-         templateUrl: '/templates/collection.html'
-       })
-   }
+  function config($stateProvider, $locationProvider) {
+   $locationProvider
+   //disable to hashbang mode like localhost:3000/#!/album
+    .html5Mode({
+    enabled: true,
+    requireBase: false
+   });
+   //add state to direct to correct template
+   $stateProvider
+    .state('landing', {
+     url: '/',
+     templateUrl: 'templates/landing.html'
+    })
+    .state('collection', {
+     url: '/collection',
+     templateUrl: 'templates/collection.html'
+    })
+    .state('album', {
+     url: '/album',
+     templateUrl: 'templates/album.html'
+    });
+  }
 
-   angular
-     .module('blocJams', ['ui.router'])
-     .config(config);
+  angular
+   .module('blocJams', ['ui.router'])
+   .config(config);
  })();
  

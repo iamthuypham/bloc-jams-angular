d45ba1b0b74ff6b839d0322cbf1289aee920b31a
Thuy Pham <tpham045@hotmail.com>
2016-11-13 18:49:08 +0000

    Adjust currentSong variable to be access publically

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,8 +1,11 @@
 (function() {
   function SongPlayer() {
     var SongPlayer = {};
-
-    var currentSong = null;
+    /**
+     * @desc Active song object from list of songs
+     * @type {Object}
+     */
+    SongPlayer.currentSong = null; //bind to SongPlayer so that it can be accessed publically
     /**
      * @desc Buzz object audio file
      * @type {Object}
@@ -17,7 +20,7 @@
       if (currentBuzzObject) { //if current Buzz file is not null 
         //*Side-note: the current Buzz file could be null in case user just landed on Album page
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong.playing = null;
       }
       //Then program updates currentSong by
       currentBuzzObject = new buzz.sound(song.audioUrl, { //Find the link to the clicked song
@@ -25,7 +28,7 @@
         preload: true
       });
 
-      currentSong = song; //Finally, set the current song to the clicked song and play it
+      SongPlayer.currentSong = song; //Finally, set the current song to the clicked song and play it
     };
     /**
      * @function playSong
@@ -33,16 +36,17 @@
      */
     var playSong = function() {
       currentBuzzObject.play();
-      currentSong.playing = true;
+      SongPlayer.currentSong.playing = true;
     }
-    
+
     SongPlayer.play = function(song) {
+      song = song || SongPlayer.currentSong;
       //For every click, program decides to play or stop a song
-      if (currentSong !== song) { //Case1: if the song was clicked is not the current playing song
-        setSong(song);// Set state for the current and clicked song
+      if (SongPlayer.currentSong !== song) { //Case1: if the song was clicked is not the current playing song
+        setSong(song); // Set state for the current and clicked song
         playSong(); //Play the clicked song
       }
-      else if (currentSong === song) { //Case2: if the song was clicked is the current playing song
+      else if (SongPlayer.currentSong === song) { //Case2: if the song was clicked is the current playing song
         if (currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
         }
@@ -50,6 +54,7 @@
     };
 
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     };

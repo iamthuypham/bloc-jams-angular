f97231e87facabafd760cc8eae80b666ca9bf499
Thuy Pham <tpham045@hotmail.com>
2016-11-13 00:50:43 +0000

    Use controller to display hero title

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,8 +1,11 @@
  (function() {
-     function LandingCtrl() {
-     }
- 
-     angular
-         .module('blocJams')
-         .controller('LandingCtrl', LandingCtrl);
+   function LandingCtrl() {
+     this.heroTitle = "Turn the Music Up!";
+
+   }
+
+   angular
+     .module('blocJams')
+     .controller('LandingCtrl', LandingCtrl);
  })();
+ 
